b9fa43f927bd26338f6799b15957cdaa92ad94b6
Adril Putra Merin <adribless37@gmail.com>
2023-04-12 18:20:27 +0700

    Added  B01 File

---
+++ B01.py
@@ -0,0 +1,13 @@
+from time import time
+rand = int(time())
+def lcg(lower_bound : int, upper_bound : int) -> int:
+    a = 1583458089
+    b = 1132489760
+    m = (2**31)-1
+    global rand
+    rand = (a*rand + b) % m
+    while rand % (upper_bound + 1) < lower_bound:
+        rand = (a*rand + b) % m
+    return rand % (upper_bound + 1)
+for i in range(10):
+    print(lcg(0,5))
