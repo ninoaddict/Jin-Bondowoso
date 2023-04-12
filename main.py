13e4c8590b8ea5a6a11371d9796b1a75f755f1d9
Adril Putra Merin <adribless37@gmail.com>
2023-04-12 19:06:40 +0700

    added command options on main file

--- main.py
+++ main.py
@@ -8,4 +8,35 @@ load()
 # run commands
 while True:
     masukan = input(">>> ")
-    # run(masukan)
+    if masukan == "login":
+        pass
+    elif masukan == "logout":
+        pass
+    elif masukan == "summonjin":
+        pass
+    elif masukan == "hapusjin":
+        pass
+    elif masukan == "ubahjin":
+        pass
+    elif masukan == "bangun":
+        pass
+    elif masukan == "kumpul":
+        pass
+    elif masukan == "batchkumpul":
+        pass
+    elif masukan == "batchbangun":
+        pass
+    elif masukan == "laporanjin":
+        pass
+    elif masukan == "laporancandi":
+        pass
+    elif masukan == "hancurkancandi":
+        pass
+    elif masukan == "ayamberkokok":
+        pass
+    elif masukan == "save":
+        pass
+    elif masukan == "help":
+        pass
+    elif masukan == "exit":
+        pass
