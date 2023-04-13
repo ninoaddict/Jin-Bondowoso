d684be43ffbb400fa61417984a2da1e91bf381db
Adril Putra Merin <adribless37@gmail.com>
2023-04-13 18:16:25 +0700

    changed global file

--- Global.py
+++ Global.py
@@ -1,15 +1,32 @@
-# const
-NMAX = 10001
+# tipe data bentukan menggunakan kelas
+class User:
+    def __init__(self, username : str, password : str, role : str) -> None:
+        self.username = username
+        self.password = password
+        self.role = role
 
-# lists
-users = [[0 for j in range(3)] for i in range(NMAX)]
-candi = [[0 for j in range(5)] for i in range(NMAX)]
-bahan_bangunan = [[0 for j in range(3)] for i in range(3)]
+class Candi:
+    def __init__(self, id : int, username : str, pasir : int, batu : int, air : int) -> None:
+        self.id = id
+        self.username = username
+        self.pasir = pasir
+        self.batu = batu
+        self.air = air
 
-# user ID and effective size of global lists initialization
+class Bahan_Bangunan:
+    def __init__(self, pasir : int, batu : int, air : int) -> None:
+        self.pasir = pasir
+        self.batu = batu
+        self.air = air
+
+class array_of_type:
+    def __init__(self, arr : list, Neff : int) -> None:
+        self.idx = arr
+        self.Neff = Neff
+
+# variable global
 ID = -1
-users_num = 0
+user = array_of_type([User(None,None,None) for i in range(1000)], 0)
+candi = array_of_type([Candi(None,None,None,None,None) for i in range(1000)], 0)
+bahan_bangunan = Bahan_Bangunan(None,None,None)
 candi_num = 0
-jinpengumpul_num = 0
-jinpembangun_num = 0
-candi_num_fixed = 0
