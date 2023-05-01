23f1bd57fe43c53ceb9eb79385abadf58bc3720b
Adril Putra Merin <adribless37@gmail.com>
2023-05-01 22:44:31 +0700

    add comments on main and global

--- Global.py
+++ Global.py
@@ -1,10 +1,12 @@
-# tipe data bentukan menggunakan kelas
+# tipe data bentukan User yang memiliki tiga parameter, yaitu username, password, dan role dari suatu user
 class User:
     def __init__(self, username : str, password : str, role : str) -> None:
         self.username = username
         self.password = password
         self.role = role
 
+# tipe data bentukan Candi yang memiliki 5 parameter, yaitu ID candi, username pembuat candi, pasir yang dibutuhkan,
+# batu yang dibutuhkan, dan air yang dibutuhkan
 class Candi:
     def __init__(self, id : int, username : str, pasir : int, batu : int, air : int) -> None:
         self.id = id
@@ -15,18 +17,20 @@ class Candi:
     def copy(self):
         return Candi(self.id,self.username,self.pasir,self.batu,self.air)
       
-
+# tipe data bentukan Bahan_Bangunan yang memiliki 3 parameter, yaitu pasir, batu, dan air yang dimiliki saat ini
 class Bahan_Bangunan:
     def __init__(self, pasir : int, batu : int, air : int) -> None:
         self.pasir = pasir
         self.batu = batu
         self.air = air
 
+# tipe data bentukan list_of_user dengan 2 parameter, yaitu list of User dan Neff (jumlah user efektif)
 class list_of_user:
     def __init__(self, arr : list[User], Neff : int) -> None:
         self.idx = arr
         self.Neff = Neff
 
+# tipe data bentukan list_of_candi dengan 2 parameter, yaitu list of Candi dan Neff (jumlah candi efektif)
 class list_of_candi:
     def __init__(self, arr : list[Candi], Neff : int) -> None:
         self.idx = arr
@@ -36,8 +40,8 @@ class list_of_candi:
         
         
 
-# variable global
+# deklarasi variabel global
 ID = -1
-user = list_of_user([User(0,0,0) for i in range(1000)], 0)
-candi = list_of_candi([Candi(0,0,0,0,0) for i in range(1000)], 0)
+user = list_of_user([User(0,0,0) for i in range(105)], 0) # banyak candi kosong dibuat sebanyak 105 
+candi = list_of_candi([Candi(0,0,0,0,0) for i in range(105)], 0) # banyak candi kosong dibuat sebanyak 105
 bahan_bangunan = Bahan_Bangunan(0,0,0)
--- main.py
+++ main.py
@@ -16,45 +16,71 @@ from F14 import save
 from F15 import help
 from F16 import exit
 
-# MAIN PROGRAM
-# load main program and files
+# memuat file user, candi, dan bahan_bangunan ke dalam variabel user, candi, dan bahan_bangunan
 load(Global.user, Global.candi, Global.bahan_bangunan)
 
-# run commands
-while True:
+# menjalankan program utama dan commands
+# Nilai ID (-1) menandakan bahwa belum ada user yang login ke dalam program
+selesai = False
+while not selesai:
     masukan = input(">>> ")
     if masukan == "login":
         login(Global.user)
     elif masukan == "logout":
         logout()
-    elif masukan == "summonjin" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+    elif masukan == "summonjin" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso": 
+        # command summonjin hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 untuk memastikan bahwa belum ada yang login ke dalam sistem karena pada bahasa python index (-1) terdefinisi sebagai elemen paling akhir
         summonjin(Global.user)
     elif masukan == "hapusjin" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+        # command hapusjin hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti diatas
         hapusjin(Global.user, Global.candi)
     elif masukan == "ubahjin" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+        # command ubahjin hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         ubahjin(Global.user)
     elif masukan == "bangun" and Global.ID != -1 and Global.user.idx[Global.ID].role == "jin_pembangun":
+        # command bangun hanya bisa diakses oleh role jin_pembangun
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         bangun(Global.user.idx[Global.ID].username, Global.candi, Global.bahan_bangunan)
     elif masukan == "kumpul" and Global.ID != -1 and Global.user.idx[Global.ID].role == "jin_pengumpul":
+        # command kumpul hanya bisa diakses oleh role jin_pengumpul
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         kumpul(Global.bahan_bangunan)
     elif masukan == "batchkumpul" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+        # command batchkumpul hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         batchkumpul(Global.bahan_bangunan, Global.user)
     elif masukan == "batchbangun" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+        # command batchbangun hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         batchbangun(Global.bahan_bangunan, Global.user, Global.candi)
     elif masukan == "laporanjin" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+        # command laporanjin hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         laporanjin(Global.user, Global.bahan_bangunan, Global.candi)
     elif masukan == "laporancandi" and Global.ID != -1 and Global.user.idx[Global.ID].role == "bandung_bondowoso":
+        # command laporancandi hanya bisa diakses oleh role bandung_bondowoso
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         laporancandi(Global.candi)
     elif masukan == "hancurkancandi" and Global.ID != -1 and Global.user.idx[Global.ID].role == "roro_jonggrang":
+        # command hancurkancandi hanya bisa diakses oleh role roro_jonggrang
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         hancurkancandi(Global.candi)
     elif masukan == "ayamberkokok" and Global.ID != -1 and Global.user.idx[Global.ID].role == "roro_jonggrang":
+        # command hancurkancandi hanya bisa diakses oleh role roro_jonggrang
+        # syarat ID != -1 dibuat dengan alasan yang sama seperti command sebelumnya
         ayamberkokok(Global.candi)
+        selesai = True
     elif masukan == "save":
         save(Global.user, Global.candi, Global.bahan_bangunan)
     elif masukan == "help":
         if Global.ID == -1:
+            # kasus jika belum ada yang login ke dalam sistem 
             help("belumlogin")
         else:
             help(Global.user.idx[Global.ID].role)
     elif masukan == "exit":
         exit(Global.user, Global.candi, Global.bahan_bangunan)
+        selesai = True
